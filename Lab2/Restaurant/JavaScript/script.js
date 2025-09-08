f0947d49f7f1744001347092f2f168ae7d6e9ea2
Wyson Kong <wyson.kong.mil@aswf.army.mil>
2025-09-08 15:48:14 -0500

    added form validation functions and bootstrap interactivity

--- Lab2/Restaurant/JavaScript/script.js
+++ Lab2/Restaurant/JavaScript/script.js
@@ -1,3 +1,5 @@
+
+<!-- Character counter for text field-->
 let infoField = document.getElementById("info");
 let charCount = document.getElementById("charCount");
 
@@ -8,8 +10,23 @@ infoField.addEventListener("input", function(count){
     charCount.textContent = `${currentLength} / ${maxLength}`;
 });
 
+let inputField = document.getElementById("hire_form");
+let userInput = inputField.querySelectorAll("input");
 
-document.getElementById("myForm").addEventListener("submit", function(event) {
+userInput.forEach(input => {
+    input.addEventListener("input", function() {
+        if (input.checkValidity()) {
+            input.classList.remove("is-invalid");
+            input.classList.add("is-valid");
+        } else {
+            input.classList.remove("is-valid");
+            input.classList.add("is-invalid");
+        }
+    })
+});
+
+<!--Checks for valid age -->
+document.getElementById("hire_form").addEventListener("submit", function(event) {
     let ageInput = document.getElementById("age").value;
     let age = Number(ageInput);
     let errors = [];
@@ -18,27 +35,31 @@ document.getElementById("myForm").addEventListener("submit", function(event) {
     if (!ageInput || isNaN(age)) {
         valid = false;
         errors.push("Enter your age")
-    } else if (age <= 21 || age >= 99) {
+    } else if (age < 21 || age > 99) {
         valid = false;
         errors.push("Enter a valid age between 21 - 99")
     }
 
     let phoneInput = document.getElementById("phone").value;
-    let phonePattern = /^\d{3}-\d{3}-\d{4}$/;
+    phoneInput = phoneInput.replaceAll(/\D/g, "");
 
-    if(!phoneInput || !phonePattern.test(phoneInput)) {
+    if(phoneInput.length !== 10) {
         valid = false;
         errors.push("Invalid number, please input your phone number ex. 123-456-7890")
+    } else {
+        phoneInput = phoneInput.replace(/(\d{3})(\d{3})(\d{4})/, "$1-$2-$3");
+        document.getElementById("phone").value = phoneInput;
     }
 
 
     if (!valid) {
         alert(errors.join("\n"))
         event.preventDefault();
-    }
-
-    let formData = new FormData(this);
-    for (let [key, value] of formData.entries()) {
-        console.log(key + ":", value);
+    } else {
+        let formData = new FormData(this);
+        for (let [key, value] of formData.entries()) {
+            console.log(key + ":", value);
+        }
     }
 });
+
