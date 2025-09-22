9b3b474366e996bf909475482103b8da05d31107
Wyson Kong <wyson.kong.mil@aswf.army.mil>
2025-09-22 09:51:08 -0500

    edit names

--- Lab3/isPalindrome/tests/isPalindrome.test.js
+++ Lab3/isPalindrome/tests/isPalindrome.test.js
@@ -3,65 +3,46 @@ let isPalindrome = require('../src/isPalindrome');
 
 describe("isPalindrome", () => {
 
-
-    describe("palindrome", () => {
         test("Palindrome should return true", () => {
             expect(isPalindrome("racecar")).toBe(true);
         });
-    })
 
-    describe("palindrome", () => {
         test("Should return true with punctation", () => {
             expect(isPalindrome("MadamI'mAdam.")).toBe(true);
         });
-    })
 
-    describe("palindrome", () => {
         test("Should return true with punctation and spaces", () => {
             expect(isPalindrome("Red rum, sir, is murder.")).toBe(true);
         });
-    })
 
-    describe("palindrome", () => {
         test("Should remove spaces and give true", () => {
             expect(isPalindrome("m u m")).toBe(true);
         });
-    })
 
-    describe("palindrome", () => {
-        test("Not a string should return false", () => {
-            expect(isPalindrome(2134)).toBe(false);
-        });
-    })
+});
 
-    describe("palindrome", () => {
-        test("Null return false", () => {
-            expect(isPalindrome(null)).toBe(false);
-        });
-    })
+describe("Not Palindrome", () => {
+    test("Not a string should return false", () => {
+        expect(isPalindrome(2134)).toBe(false);
+    });
 
-    describe("palindrome", () => {
-        test("Boolean return false", () => {
-            expect(isPalindrome(true)).toBe(false);
-        });
-    })
+    test("Null return false", () => {
+        expect(isPalindrome(null)).toBe(false);
+    });
 
-    describe("palindrome", () => {
-        test("Boolean return false", () => {
-            expect(isPalindrome(false)).toBe(false);
-        });
-    })
+    test("Boolean return false", () => {
+        expect(isPalindrome(true)).toBe(false);
+    });
 
-    describe("palindrome", () => {
-        test("No argument return false", () => {
-            expect(isPalindrome("")).toBe(false);
-        });
-    })
+    test("Boolean return false", () => {
+        expect(isPalindrome(false)).toBe(false);
+    });
 
-    describe("palindrome", () => {
-        test("Not a palindrome should return false", () => {
-            expect(isPalindrome("apple")).toBe(false);
-        });
-    })
+    test("No argument return false", () => {
+        expect(isPalindrome("")).toBe(false);
+    });
 
+    test("Not a palindrome should return false", () => {
+        expect(isPalindrome("apple")).toBe(false);
+    });
 });
