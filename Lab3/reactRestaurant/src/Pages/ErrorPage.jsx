391c98438ab464d5b4f19cb11239351727870b5d
Wyson Kong <wyson.kong.mil@aswf.army.mil>
2025-10-01 16:41:59 -0500

    updated background and fixed link

--- Lab3/reactRestaurant/src/Pages/ErrorPage.jsx
+++ Lab3/reactRestaurant/src/Pages/ErrorPage.jsx
@@ -1,6 +1,7 @@
 import React from "react";
-import { useNavigate } from 'react-router-dom';
+import {Link, useNavigate} from 'react-router-dom';
 import HomePage from "./HomePage.jsx";
+import background from "../assets/malaysian-food.jpg";
 
 const returnHome = () => {
     const navigate = useNavigate();
@@ -9,15 +10,12 @@ const returnHome = () => {
 
 const ErrorPage = () => {
     return (
-        <div className="bg-no-repeat bg-center bg-cover h-screen flex items-center justify-center">
+        <div style={{ backgroundImage: `url(${background})`}} className="bg-no-repeat bg-center bg-cover h-screen flex items-center justify-center">
             <div className="bg-black bg-opacity-50 p-8 rounded-lg text-center text-white opacity-75">
                 <h1 className="text-4xl font-bold mb-4">Error 404</h1>
                 <p className="text-xl">We are working on the page.</p>
-                <button
-                    onClick={returnHome}
-                    className="bg-gray-700 text-white px-6 py-2 rounded hover:bg-gray-800 transition-colors"
-                >
-                    Return Home
+                <button className="bg-gray-700 text-white px-6 py-2 rounded hover:bg-gray-800 transition-colors">
+                    <Link to="/">Return Home</Link>
                 </button>
             </div>
         </div>
